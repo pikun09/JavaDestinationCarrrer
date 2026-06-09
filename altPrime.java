49c036136c5ddce43b40785e11186cb6f4210f73
pikun09 <pikun9875@gmail.com>
2026-06-09 20:12:50 +0530

    Add altPrime class to identify alternating prime numbers

--- altPrime.java
+++ altPrime.java
@@ -1,3 +1,33 @@
 public class altPrime {
-    
+    public static boolean isPrime(int num){
+        if (num == 1 || num == 0){
+            return false;
+        } 
+        for(int i = 2 ;i*i<=num;i++){
+            if(num  % i ==0 ){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        int count = 0;
+        int i = 2;
+        int range = 10;
+        while(range!=0){
+            if(isPrime(i)){
+                if(count == 0){
+                    System.out.println(i);
+                    
+                    count = 1;
+                    range -- ;
+                }else{
+                    count = 0;
+                }
+            }
+            i++;
+
+        }
+
+    }
 }
