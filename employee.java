865608224d4a836099d4478e66ace1909ee2fb68
pikun09 <pikun9875@gmail.com>
2026-06-18 16:15:44 +0530

    Add employee class with constructors and main method for testing

---
+++ employee.java
@@ -0,0 +1,18 @@
+public class employee{
+    int id;
+    String name;
+    {
+        System.out.println("Employee object created");
+        System.out.println(name);
+    }
+    employee(int id,String name){
+        this.id = id ;
+        this.name = name;
+
+    }
+    public static void main(String[] args){
+        employee e1 = new employee(1, "Payal");
+        System.out.println(e1.id);
+        System.out.println(e1.name);
+    }
+}
